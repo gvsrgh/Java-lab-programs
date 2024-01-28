6b5f6689761ea1e23c6ee5bd423fc9271c254208
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-01-28 16:41:46 +0530

    ArmStrongNumber
    Let a number x containing n digits. If sum of each digit raised to power n is equal to the number
    itself, then the number is said to be Armstrong number i.e. Now your task to check whether the
    given number is Armstrong number or not.

--- Question23A.java
+++ Question23A.java
@@ -2,7 +2,7 @@ package lab02;
 
 import java.util.Scanner;
 
-public class Question03A {
+public class Question23A {
 	public static void main(String args[])
 	{
 		System.out.println("Enter the number: ");
